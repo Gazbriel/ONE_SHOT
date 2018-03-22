4b706fad4d6763391e5eb941a1d569f4cd847b59
Gazbriel <xroberg@gmail.com>
2018-03-22 23:03:47 +0100

    Fixed some bugs
    Fixed bartender dialog.
    Fixed some translation.
    Added inputs A and S.

--- Assets/Prephabs/Characters/BarTender/BartenderInteractionDialog.cs
+++ Assets/Prephabs/Characters/BarTender/BartenderInteractionDialog.cs
@@ -4,12 +4,14 @@ using UnityEngine;
 
 public class BartenderInteractionDialog : MonoBehaviour {
 
+    private bool dialog1Done;
     private void OnCollisionEnter2D(Collision2D collision)
     {
-        if (collision.gameObject.name == "Main character" && GameObject.Find("Game State").GetComponent<CoreGameState>().barTenderSpeech)
+        if (collision.gameObject.name == "Main character" && GameObject.Find("Game State").GetComponent<CoreGameState>().barTenderSpeech && !dialog1Done)
         {
             //player.gameObject.transform.localScale = new Vector3(-1, transform.localScale.y, transform.localScale.z);
             GetComponent<ShowDialogController>().ChangeToDialog(1);
+            dialog1Done = true;
         }
     }
 }
